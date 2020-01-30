19222d705e68518283b9c9f0c73ee3acb825972e
Diogo Pavan <diogopv10@gmail.com>
2020-01-29 22:56:22 -0300

    FLuxo para criar sessao e criar mensagem

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const AssistantV2 = require('ibm-watson/assistant/v2');
 const { IamAuthenticator } = require('ibm-watson/auth');
+let sessionId = null;
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,36 +16,51 @@ const assistant = new AssistantV2({
   url: process.env.ASSISTANT_URL,
 });
 
-app.post('/conversation/', (req, res) => {
-  const { text, context = {}, message_type = 'text' } = req.body;
+const sendMessage = async (data, response) => {
+  const { text, context = {}, message_type = 'text' } = data;
 
+  await assistant
+    .message({
+      assistantId: process.env.ASSISTANT_ID,
+      sessionId,
+      context,
+      input: {
+        message_type,
+        text,
+      },
+    })
+    .then(res => {
+      console.log(JSON.stringify(res, null, 2));
+      response.json(res.result);
+    })
+    .catch(err => {
+      console.log(err);
+      sessionId = null;
+      createSession(data, response);
+    });
+};
+
+const createSession = (data, response) => {
   assistant
     .createSession({
       assistantId: process.env.ASSISTANT_ID,
     })
     .then(res => {
-      const { session_id: sessionId } = res.result;
-
-      assistant
-        .message({
-          assistantId: process.env.ASSISTANT_ID,
-          sessionId,
-          context,
-          input: {
-            message_type,
-            text,
-          },
-        })
-        .then(res => {
-          console.log(JSON.stringify(res, null, 2));
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      sessionId = res.result.session_id;
+      sendMessage(data, response);
     })
     .catch(err => {
       console.log(err);
+      response.json(err);
     });
+};
+
+app.post('/conversation/', (req, response) => {
+  if (!sessionId) {
+    createSession(req.body, response);
+  } else {
+    sendMessage(req.body, response);
+  }
 });
 
 app.listen(process.env.APP_PORT, () =>
